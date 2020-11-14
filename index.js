fac3e1caec91c0c3909c2633dd3bb62e3470fa5e
Shrinjay <shrinjaymukherjee@gmail.com>
2020-11-13 19:36:04 -0700

    Fixed route

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.use('/', (req, res, next)=>{
     else next()
 })
 
-app.use('/', express.static('../client'))
+app.use('/', express.static('./client'))
 
 const DigestRouter = require('./routes/Digest')
 app.use('/api/digest', DigestRouter)
